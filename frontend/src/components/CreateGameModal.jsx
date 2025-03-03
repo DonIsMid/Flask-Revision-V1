97c0a31c6a54973847806270c0d3f7a729dad0a9
LazY <145108769+DonIsMid@users.noreply.github.com>
2025-03-03 12:01:36 +0000

    modal added to api

--- frontend/src/components/CreateGameModal.jsx
+++ frontend/src/components/CreateGameModal.jsx
@@ -25,6 +25,21 @@ import { BiAddToQueue } from "react-icons/bi";
 
 const CreateGameModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [inputs, setInputs] = React.useState({
+    title: "",
+    genre: "",
+    category: "",
+    description: "",
+    image_url: "",
+  });
+  const handleCreateGame = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:5000/api/games", {
+
+    }
+  };
 
   return (
     <>
@@ -34,7 +49,11 @@ const CreateGameModal = () => {
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
+
+        <form onSubmit={handleCreateGame}>
+
         <ModalContent bg={useColorModeValue("brand.600", "brand.500")} color={useColorModeValue("brand.900", "brand.800")}>
+
           <ModalHeader>New Game 🔥</ModalHeader>
           <ModalCloseButton />
 
@@ -42,12 +61,16 @@ const CreateGameModal = () => {
             <Flex alignItems={"center"} gap={4}>
               <FormControl>
                 <FormLabel>Full Title</FormLabel>
-                <Input placeholder="Game Name" />
+                <Input placeholder="Game Name" 
+                value={inputs.title}
+                onChange={(e) => setInputs({...inputs, title: e.target.value})}/>
               </FormControl>
 
               <FormControl>
                 <FormLabel>Genre</FormLabel>
-                <Input placeholder="Game Genre" />
+                <Input placeholder="Game Genre" 
+                value={inputs.genre}
+                onChange={(e) => setInputs({...inputs, genre: e.target.value})}/>
               </FormControl>
             </Flex>
 
@@ -57,14 +80,22 @@ const CreateGameModal = () => {
                 resize={"none"}
                 overflow={"hidden"}
                 placeholder="What's the game about?"
+                value={inputs.description}
+                onChange={(e) => setInputs({...inputs, description: e.target.value})}
               />
             </FormControl>
 
             <RadioGroup mt={4}>
               <Flex gap={5}>
-                <Radio value="aaa">AAA</Radio>
-                <Radio value="aa">AA</Radio>
-                <Radio value="indie">Indie</Radio>
+                <Radio value="aaa"
+                onChange={(e) => setInputs({...inputs, category: e.target.value})}
+                >AAA</Radio>
+                <Radio value="aa"
+                onChange={(e) => setInputs({...inputs, category: e.target.value})}
+                >AA</Radio>
+                <Radio value="indie"
+                onChange={(e) => setInputs({...inputs, category: e.target.value})}
+                >Indie</Radio>
               </Flex>
             </RadioGroup>
 
@@ -74,18 +105,24 @@ const CreateGameModal = () => {
                 resize={"none"}
                 overflow={"hidden"}
                 placeholder="https://upload.wikimedia.org/wikipedia/commons/d/d5/Nintendo-Switch-Pro-Controller-FL.jpg"
+                value={inputs.image_url}
+                onChange={(e) => setInputs({...inputs, image_url: e.target.value})}
               />
             </FormControl>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
+            <Button colorScheme="blue" mr={3} type="submit">
               Add
             </Button>
             <Button colorScheme="red" mr={3}>
               Cancel
             </Button>
           </ModalFooter>
+
         </ModalContent>
+
+        </form>
+
       </Modal>
     </>
   );
